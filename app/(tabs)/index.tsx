238c25c601821a4d47e2abb7d42420e07c446f87
Anh Nguyen Duy <anhnd4@smartosc.com>
2023-06-22 17:47:20 +0700

    apply style and function for component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,37 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Button, TextInput, ScrollView } from 'react-native';
+import { useState } from 'react';
 
-import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 
 export default function TabOneScreen() {
+  const [enterGoalText, setEnterGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState<string[]>([]);
+  const goalInputHandler = (enterText: string) => {
+    setEnterGoalText(enterText);
+  };
+
+  const addGoalInputHandler = () => {
+    setCourseGoals((currentGoals) => [...currentGoals, enterGoalText]);
+    setEnterGoalText('');
+  };
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <View style={styles.inputContainer}>
+        <TextInput
+          value={enterGoalText}
+          style={styles.textInput}
+          placeholder='Your course goal'
+          onChangeText={goalInputHandler}
+        />
+        <Button title='Click me' onPress={addGoalInputHandler} />
+      </View>
+      <View style={styles.goalsContainer}>
+        <ScrollView>
+          {courseGoals.map((item: string) => (
+            <Text style={styles.goalItem}>{item}</Text>
+          ))}
+        </ScrollView>
+      </View>
     </View>
   );
 }
@@ -19,13 +42,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingBottom: 24,
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  textInput: {
+    width: '70%',
+    borderWidth: 1,
+    marginRight: 8,
+  },
+  goalsContainer: {
+    width: '100%',
+    flex: 5,
+  },
+  goalItem: {
+    backgroundColor: '#5e0acc',
+    padding: 8,
+    borderRadius: 5,
+    margin: 8,
   },
 });
